3ef4ec101d0309db85a5769a01f660db1f654db5
kovacikk <kovacikkyl@gmail.com>
2020-03-27 13:49:39 -0400

    Added /fish and images

--- index.js
+++ index.js
@@ -2,10 +2,20 @@ const express = require('express')
 const app = express()
 const port = 8082
 
+const path = require('path');
+app.use(express.static(__dirname+'/'));
+
+//Default
 app.get('/', (req, res) => res.send('Hello World!'))
+
+//Spaghetti
 app.get('/spaghetti', function(req, res) {
 	res.send('You are now accessing the spaghetti page!')
 })
 
+//Fish
+app.get('/fish', function(req, res) {
+	res.sendFile(path.join(__dirname+'/html/fish.html'));
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
