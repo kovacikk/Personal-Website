1090a17aee926e6f7e75de76d16f89b26288bef1
kovacikk <kovacikkyl@gmail.com>
2020-04-12 15:26:05 -0400

    Added gamer files to test custom modules

--- app/index.js
+++ app/index.js
@@ -3,6 +3,8 @@ const app = express()
 const port = 8080
 const https = require('https')
 
+var gamerTime = require('./utils.js');
+
 const path = require('path');
 app.use(express.static(__dirname+'/'));
 app.use(express.json());
@@ -20,7 +22,7 @@ app.get('/spaghetti', function(req, res) {
 	res.send('You are now accessing the spaghetti page!\n')
 })
 
-//fish
+//fishW
 app.get('/fish', function(req, res) {
 	res.sendFile(path.join(__dirname+'/html/fish.html'));
 })
@@ -34,6 +36,11 @@ app.get('/getMessage', function(req, res) {
 app.post('/setMessage', function (req, res) {
 	memory = req.body.message;
 	res.send('POST request\n\"' + memory + "\" recieved!\n");
+})
+
+app.get('/gamer', function(req, res) {
+	var gamer = gamerTime.gamerTime()
+	res.send(gamer);
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
---
+++ app/utils.js
@@ -0,0 +1,5 @@
+//utils.js
+
+exports.gamerTime = function() {
+    return "it is game time\n";
+};
