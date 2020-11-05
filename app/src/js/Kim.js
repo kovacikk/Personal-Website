c93d3960f6620c03264ec3667521053e39cc5346
kovacikk <kovacikkyl@gmail.com>
2020-11-05 15:55:57 -0500

    Finished KimBd

--- app/src/js/Kim.js
+++ app/src/js/Kim.js
@@ -23,6 +23,8 @@ class Kim extends Component {
            dad8: <p></p>,
            dad9clicked: false,
            dad9: <p></p>,
+           dad10clicked: false,
+           dad10: <p></p>,
         }
     };
 
@@ -71,6 +73,11 @@ class Kim extends Component {
             dad9clicked: true
         });
     }
+    dad10click = () => {
+        this.setState({
+            dad10clicked: true
+        });
+    }
 
    
     render() {
@@ -81,72 +88,81 @@ class Kim extends Component {
                     </video>
         }
         else {
-            this.dad1 = <img onClick={this.dad1click} src={require('../img/dad2_256.jpg')}></img>
+            this.dad1 = <img class="link" onClick={this.dad1click} src={require('../img/dad2_256.jpg')}></img>
         }
         if (this.state.dad2clicked) {
             this.dad2 = <video controls autoPlay>
-                    <source src={require('../img/dadDame.mp4')} type="video/mp4"></source> 
+                    <source src={require('../img/yourMomFinished.mp4')} type="video/mp4"></source> 
                     </video>
         }
         else {
-            this.dad2 = <img onClick={this.dad2click} src={require('../img/dad2_256.jpg')}></img>
+            this.dad2 = <img class="link" onClick={this.dad2click} src={require('../img/mom2_256.jpg')}></img>
         }
         if (this.state.dad3clicked) {
             this.dad3 = <video controls autoPlay>
-                    <source src={require('../img/dadDame.mp4')} type="video/mp4"></source> 
+                    <source src={require('../img/harleyBigTime.mp4')} type="video/mp4"></source> 
                     </video>
         }
         else {
-            this.dad3 = <img onClick={this.dad3click} src={require('../img/dad2_256.jpg')}></img>
+            this.dad3 = <img class="link" onClick={this.dad3click} src={require('../img/harley256.jpg')}></img>
         }
         if (this.state.dad4clicked) {
             this.dad4 = <video controls autoPlay>
-                    <source src={require('../img/dadDame.mp4')} type="video/mp4"></source> 
+                    <source src={require('../img/momLife.mp4')} type="video/mp4"></source> 
                     </video>
         }
         else {
-            this.dad4 = <img onClick={this.dad4click} src={require('../img/dad2_256.jpg')}></img>
+            this.dad4 = <img class="link" onClick={this.dad4click} src={require('../img/mom256.jpg')}></img>
         }
         if (this.state.dad5clicked) {
             this.dad5 = <video controls autoPlay>
-                    <source src={require('../img/dadDame.mp4')} type="video/mp4"></source> 
+                    <source src={require('../img/fligFinished.mp4')} type="video/mp4"></source> 
                     </video>
         }
         else {
-            this.dad5 = <img onClick={this.dad5click} src={require('../img/dad2_256.jpg')}></img>
+            this.dad5 = <img class="link" onClick={this.dad5click} src={require('../img/kim256.jpg')}></img>
         }
         if (this.state.dad6clicked) {
             this.dad6 = <video controls autoPlay>
-                    <source src={require('../img/dadDame.mp4')} type="video/mp4"></source> 
+                    <source src={require('../img/yourDad.mp4')} type="video/mp4"></source> 
                     </video>
         }
         else {
-            this.dad6 = <img onClick={this.dad6click} src={require('../img/dad2_256.jpg')}></img>
+            this.dad6 = <img class="link" onClick={this.dad6click} src={require('../img/dad256.jpg')}></img>
         }
         if (this.state.dad7clicked) {
             this.dad7 = <video controls autoPlay>
-                    <source src={require('../img/dadDame.mp4')} type="video/mp4"></source> 
+                    <source src={require('../img/yourDogFinished.mp4')} type="video/mp4"></source> 
                     </video>
         }
         else {
-            this.dad7 = <img onClick={this.dad7click} src={require('../img/dad2_256.jpg')}></img>
+            this.dad7 = <img class="link" onClick={this.dad7click} src={require('../img/harley256.jpg')}></img>
         }
         if (this.state.dad8clicked) {
             this.dad8 = <video controls autoPlay>
-                    <source src={require('../img/dadDame.mp4')} type="video/mp4"></source> 
+                    <source src={require('../img/dadOcean.mp4')} type="video/mp4"></source> 
                     </video>
         }
         else {
-            this.dad8 = <img onClick={this.dad8click} src={require('../img/dad2_256.jpg')}></img>
+            this.dad8 = <img class="link" onClick={this.dad8click} src={require('../img/dad256.jpg')}></img>
         }
         if (this.state.dad9clicked) {
             this.dad9 = <video controls autoPlay>
-                    <source src={require('../img/dadDame.mp4')} type="video/mp4"></source> 
+                    <source src={require('../img/yourYouFinished.mp4')} type="video/mp4"></source> 
                     </video>
         }
         else {
-            this.dad9 = <img onClick={this.dad9click} src={require('../img/dad2_256.jpg')}></img>
+            this.dad9 = <img class="link" onClick={this.dad9click} src={require('../img/kim256.jpg')}></img>
         }
+        if (this.state.dad10clicked) {
+            this.dad10 = <video controls autoPlay>
+                    <source src={require('../img/dadNuma.mp4')} type="video/mp4"></source> 
+                    </video>
+        }
+        else {
+            this.dad10 = <img class="link" onClick={this.dad10click} src={require('../img/dad256.jpg')}></img>
+        }
+        
     
 
         return (
@@ -155,14 +171,11 @@ class Kim extends Component {
                 <br></br>
 			    <p>Another Birthday for Another Kim,<br></br> I hope your excited for an exciting night.</p>
                 <br></br>
-                <p>Since I rounded up a couple of your family members,<br></br> who will all start this party right!!!!</p>
+                <p>I rounded up a couple of your family members,<br></br> who will all start this party right!!!!</p>
 
                 <br></br>
                 <p>Click on the images of the members below for a special message from them:</p>  
 
-                <p>
-                        
-                    </p>
 
 
                 <table>
@@ -201,10 +214,9 @@ class Kim extends Component {
                     </tr>
                 </table>
                 
-
-                <br></br>
-
-                <p id="endcard">Happy Birthday Kim from Kyle</p>
+                <p>Bonus Dad (Personal Favorite):</p>
+            <table><tr><th>{this.dad10}</th></tr></table>
+            
             </div>
         );
     }
