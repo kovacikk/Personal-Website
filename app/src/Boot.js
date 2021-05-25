6a33b01ef76295c8b5ae634ab322d15a60f7f582
kovacikk <kovacikkyl@gmail.com>
2021-05-24 20:50:10 -0400

    Slight Routing and moved files

---
+++ app/src/Boot.js
@@ -0,0 +1,207 @@
+import React, { Component, Fragment } from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
+import './css/Boot.css';
+import Home from './js/Home.js';
+import About from './js/About.js';
+import Forum from './js/Forum.js';
+import Contact from './js/Contact.js';
+import Serve from './js/Serve.js';
+import Music from './js/Music.js';
+
+class Boot extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            
+        }
+    };
+
+    render() {
+        return (
+            
+            <div id="overall" class="container-fluid">
+                <Router>
+                <div class="row">
+                    <div class="col-sm-4 col-md-3 col-lg-2 gy-5 gx-5" id="sidebar">
+                        
+                        <div class="row gx-1 p-3 text-light bg-dark sidebar">  
+                            <a class="align-items-center text-light text-decoration-none" data-bs-toggle="collapse" href="#collapseSidebar" aria-expanded="true">
+                                <span class="fs-4">Links</span>
+                            </a>
+                            <hr/>
+                            
+                            <ul class="nav nav-pills flex-column collapse show" id="collapseSidebar" aria-expanded="false">
+                            <Switch>
+                                    <Route path="/projects">
+                                    <li class="nav-item">
+                                        <a href="/" class="nav-link text-light" aria-current="page">
+                                            Home
+                                        </a>
+                                        </li>
+                                        <li>
+                                        <a href="/projects" class="nav-link text-light active">
+                                            Projects
+                                        </a>
+                                        </li>
+                                        <li>
+                                        <a href="/music" class="nav-link text-light">
+                                            Music
+                                        </a>
+                                        </li>
+                                        <li>
+                                        <a href="/about" class="nav-link text-light">
+                                            About
+                                        </a>
+                                        </li>
+                                    </Route>
+                                    <Route path="/music">
+                                    <li class="nav-item">
+                                        <a href="/" class="nav-link text-light" aria-current="page">
+                                            Home
+                                        </a>
+                                        </li>
+                                        <li>
+                                        <a href="/projects" class="nav-link text-light">
+                                            Projects
+                                        </a>
+                                        </li>
+                                        <li>
+                                        <a href="/music" class="nav-link text-light active">
+                                            Music
+                                        </a>
+                                        </li>
+                                        <li>
+                                        <a href="/about" class="nav-link text-light">
+                                            About
+                                        </a>
+                                        </li>
+                                    </Route>
+                                    <Route path="/about">
+                                        <li class="nav-item">
+                                        <a href="/" class="nav-link text-light" aria-current="page">
+                                            Home
+                                        </a>
+                                        </li>
+                                        <li class="nav-item">
+                                        <a href="/projects" class="nav-link text-light">
+                                            Projects
+                                        </a>
+                                        </li>
+                                        <li class="nav-item">
+                                        <a href="/music" class="nav-link text-light">
+                                            Music
+                                        </a>
+                                        </li>
+                                        <li class="nav-item">
+                                        <a href="/about" class="nav-link text-light active">
+                                            About
+                                        </a>
+                                        </li>
+                                    </Route>
+                                    <Route path="/">
+                                        <li class="nav-item">
+                                        <a href="/" class="nav-link active" aria-current="page">
+                                            Home
+                                        </a>
+                                        </li>
+                                        <li>
+                                        <a href="/projects" class="nav-link text-light">
+                                            Projects
+                                        </a>
+                                        </li>
+                                        <li>
+                                        <a href="/music" class="nav-link text-light">
+                                            Music
+                                        </a>
+                                        </li>
+                                        <li>
+                                        <a href="/about" class="nav-link text-light">
+                                            About
+                                        </a>
+                                        </li>
+                                    </Route>
+                                </Switch>
+                                <hr/>
+                            </ul>
+                            
+                        </div>
+                        <div class="row p-3"></div>
+
+                    </div>
+                    <div class="col" id="center">
+                        <div class="container d-flex flex-column h-100">
+                            <div class="row align-items-start" id="header">
+                                <nav class="navbar navbar-expand navbar-dark bg-dark gy-2">
+                                    <div class="container-fluid">
+                                            <div class="collapse navbar-collapse justify-content-md-center">
+                                                <ul class="navbar-nav">
+                                                    <li class="nav-item">
+                                                        <h2><a class="nav-link active text-light" href="/" id="headerText">Kyle Kovacik</a></h2>
+                                                    </li>
+                                                </ul>
+                                            </div>
+                                    </div>
+                                </nav>
+                            </div>
+                            <div class="row align-items-center">
+                                <div class="col">
+                                
+                                <Switch>
+                                    <Route path="/about">
+                                        <About/>
+                                    </Route>
+                                    <Route path="/forum">
+                                        <Forum/>
+                                    </Route>
+                                    <Route path="/contact">
+                                        <Contact/>
+                                    </Route>
+                                    <Route path="/serve/">
+                                        <Serve/>
+                                    </Route>
+                                    <Route path="/music">
+                                        <Music/>
+                                    </Route>
+                                    <Route path="/">
+                                        <Home />
+                                    </Route>
+                                </Switch>
+                             
+
+                                </div>
+                            </div>
+                            <div class="row" id="spacer">
+
+                            </div>
+                            <div class="row mt-auto mb-2" id="footer">
+                                <footer class="footer mt-auto py-3 bg-dark gy-5" id="footer">
+                                    <div class="container-fluid">
+                                        <div class="row bottom-align-text">
+                                            <div class="col" align="center">
+                                                <p class="text-light mb-1">For more information, check out my Github and LinkedIn pages in the About Tab</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </footer>
+                            </div>
+                            <div class="row" id="footerSpace">
+                            
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                </Router>
+            </div>
+
+
+    
+        );
+    }
+}
+
+export default Boot;
--- app/src/index.js
+++ app/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App.js';
-import Boot from './js/Boot.js';
+import Boot from './Boot.js';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css'
 require('@popperjs/core');
